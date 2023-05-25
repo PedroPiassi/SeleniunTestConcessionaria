10cd8ad1a495bc848b3ca506168bb7054018871e
Pedro.Piassi <pedro.a.piassi@gmail.com>
2023-05-25 11:07:49 -0300

    Feat: configure setup to tests

---
+++ src/test/java/br/edu/ifsp/scl/tc1/MainTest.java
@@ -0,0 +1,34 @@
+package br.edu.ifsp.scl.tc1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+class MainTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    void shouldOpenConcessionariaPage() throws InterruptedException {
+        driver.get("http://localhost:5173");
+        Thread.sleep(1000);
+    }
+
+
+}
