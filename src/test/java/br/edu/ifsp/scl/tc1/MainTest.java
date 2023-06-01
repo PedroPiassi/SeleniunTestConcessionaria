9c134d6a97a259e81355203db73981c2c39cfd2f
Pedro.Piassi <pedro.a.piassi@gmail.com>
2023-06-01 10:36:42 -0300

    Test: Add Display name in first test

--- src/test/java/br/edu/ifsp/scl/tc1/MainTest.java
+++ src/test/java/br/edu/ifsp/scl/tc1/MainTest.java
@@ -3,10 +3,18 @@ package br.edu.ifsp.scl.tc1;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.Duration;
 
 class MainTest {
     private WebDriver driver;
@@ -25,9 +33,10 @@ class MainTest {
     }
 
     @Test
+    @DisplayName("Should open Concessionaria page")
     void shouldOpenConcessionariaPage() throws InterruptedException {
         driver.get("http://localhost:5173");
-        Thread.sleep(1000);
+        Thread.sleep(Duration.ofSeconds(2));
     }
 
 
