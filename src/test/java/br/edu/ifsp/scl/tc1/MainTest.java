eaae3117ff01dfe6438ed71447120a133218dccb
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-06-14 09:49:12 -0300

    Test: Add should alert if year is null in create form

--- src/test/java/br/edu/ifsp/scl/tc1/MainTest.java
+++ src/test/java/br/edu/ifsp/scl/tc1/MainTest.java
@@ -352,5 +352,20 @@ class MainTest {
 
             assertThat(alert.isDisplayed()).isTrue();
         }
+
+        @Test
+        @DisplayName("Should alert if year is null in create form")
+        void shouldAlertIfYearIsNullInCreateForm() {
+            driver.get(CREATE_PAGE_URL);
+
+            final WebElement button = new WebDriverWait(driver, Duration.ofSeconds(2))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/form/button")));
+            button.click();
+
+            final WebElement alert = new WebDriverWait(driver, Duration.ofSeconds(2))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/form/div[1]/div/div[2]/div[2]")));
+
+            assertThat(alert.isDisplayed()).isTrue();
+        }
     }
 }
