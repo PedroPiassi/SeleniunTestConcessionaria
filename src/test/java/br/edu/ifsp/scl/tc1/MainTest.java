9a2f2c6b6658cac1852d2276a02639ff348f3974
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-06-14 10:04:31 -0300

    Test: Add should alert if description is null in create form

--- src/test/java/br/edu/ifsp/scl/tc1/MainTest.java
+++ src/test/java/br/edu/ifsp/scl/tc1/MainTest.java
@@ -522,5 +522,20 @@ class MainTest {
 
             assertThat(alert.isDisplayed()).isTrue();
         }
+
+        @Test
+        @DisplayName("Should alert if description is null in create form")
+        void shouldAlertIfDescriptionIsNullInCreateForm() {
+            driver.get(CREATE_PAGE_URL);
+
+            final WebElement button = new WebDriverWait(driver, Duration.ofSeconds(2))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/form/button")));
+            button.click();
+
+            final WebElement alert = new WebDriverWait(driver, Duration.ofSeconds(2))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/form/div[4]/div[2]")));
+
+            assertThat(alert.isDisplayed()).isTrue();
+        }
     }
 }
