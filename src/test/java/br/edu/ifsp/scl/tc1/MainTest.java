27da12c595c90857145db2605c3a0ed2815d8955
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-06-14 09:51:24 -0300

    Test: Add should alert if year is higher than actual year in create form

--- src/test/java/br/edu/ifsp/scl/tc1/MainTest.java
+++ src/test/java/br/edu/ifsp/scl/tc1/MainTest.java
@@ -367,5 +367,24 @@ class MainTest {
 
             assertThat(alert.isDisplayed()).isTrue();
         }
+
+        @Test
+        @DisplayName("Should alert if year is higher than actual year in create form")
+        void shouldAlertIfYearIsHigherInCreateForm() {
+            driver.get(CREATE_PAGE_URL);
+
+            final WebElement input = new WebDriverWait(driver, Duration.ofSeconds(2))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"year\"]")));
+            input.sendKeys("2050");
+
+            final WebElement button = new WebDriverWait(driver, Duration.ofSeconds(2))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/form/button")));
+            button.click();
+
+            final WebElement alert = new WebDriverWait(driver, Duration.ofSeconds(2))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/form/div[1]/div/div[2]/div[2]")));
+
+            assertThat(alert.isDisplayed()).isTrue();
+        }
     }
 }
