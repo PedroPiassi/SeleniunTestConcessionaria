0c68192addf5f6c0f8a173e87898c1bf8fd8a479
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-06-14 09:58:32 -0300

    Test: Add should alert if warranty is blank in create form

--- src/test/java/br/edu/ifsp/scl/tc1/MainTest.java
+++ src/test/java/br/edu/ifsp/scl/tc1/MainTest.java
@@ -435,5 +435,24 @@ class MainTest {
 
             assertThat(alert.isDisplayed()).isTrue();
         }
+
+        @Test
+        @DisplayName("Should alert if warranty is blank in create form")
+        void shouldAlertIfWarrantyIsBlankInCreateForm() {
+            driver.get(CREATE_PAGE_URL);
+
+            final WebElement input = new WebDriverWait(driver, Duration.ofSeconds(2))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"warranty\"]")));
+            input.sendKeys(" ");
+
+            final WebElement button = new WebDriverWait(driver, Duration.ofSeconds(2))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/form/button")));
+            button.click();
+
+            final WebElement alert = new WebDriverWait(driver, Duration.ofSeconds(2))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/form/div[2]/div/div[3]/div[2]")));
+
+            assertThat(alert.isDisplayed()).isTrue();
+        }
     }
 }
